c24a23bd2ce9a3c602eb1a0bb1a6ba0ba18821a3
Paul M Fox <paul@homemadedigital.com>
2016-11-08 11:55:43 +0000

    Proper variable escaping; backtick literals

--- scope_test.go
+++ scope_test.go
@@ -28,8 +28,8 @@ func Test_AValidScopeCanInterpolateVariables(t *testing.T) {
 				"one": "value1",
 				"two": "value2",
 			},
-			literal: `\$one is $two, \\$var is escaped`,
-			result:  `$one is value2, \$var is escaped`,
+			literal: `\$one is $two, \\ is escaped`,
+			result:  `$one is value2, \ is escaped`,
 		},
 		{
 			variables: map[string]string{
@@ -38,6 +38,30 @@ func Test_AValidScopeCanInterpolateVariables(t *testing.T) {
 			},
 			literal: `${one}_${two}`,
 			result:  `value1_value2`,
+		},
+		{
+			variables: map[string]string{
+				"one": "value1",
+				"two": "value2",
+			},
+			literal: `${one}${two}`,
+			result:  `value1value2`,
+		},
+		{
+			variables: map[string]string{
+				"one": "value1",
+				"two": "value2",
+			},
+			literal: `${one`,
+			err:     fmt.Errorf("Expecting closing right brace in variable ${one}"),
+		},
+		{
+			variables: map[string]string{
+				"one": "value1",
+				"two": "value2",
+			},
+			literal: `${one is unfinished`,
+			err:     fmt.Errorf("Expecting closing right brace in variable ${one}"),
 		},
 		{
 			variables: map[string]string{
@@ -65,7 +89,28 @@ func Test_AValidScopeCanInterpolateVariables(t *testing.T) {
 			variables: map[string]string{},
 			err:       fmt.Errorf("Unknown variable '$nothing'"),
 			literal:   `something = $nothing`,
-			result:    `something = $nothing`,
+			result:    `something = `,
+		},
+		{
+			variables: map[string]string{},
+			literal:   `This ends in a \`,
+			result:    `This ends in a \`,
+		},
+		{
+			variables: map[string]string{},
+			literal:   "This is a `$confusing $literal \\`",
+			result:    "This is a $confusing $literal \\",
+		},
+		{
+			variables: map[string]string{},
+			literal:   "This is \\`not a literal\\`",
+			result:    "This is `not a literal`",
+		},
+		{
+			variables: map[string]string{},
+			literal:   "This is `not a literal",
+			result:    "This is not a literal",
+			err:       fmt.Errorf("Unterminated backtick literal"),
 		},
 	} {
 		t.Logf("Cycle %d", cycle)
